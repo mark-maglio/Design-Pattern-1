c8631212a4616fb5dd25dbedff2950339b601b44
mark maglio <markmaglio25@gmail.com>
2021-03-02 18:03:10 +0100

    Aggiungere i file di progetto.

---
+++ Design Pattern 1/Cella.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_1
+{
+    public class Cella
+    {
+        internal Cella() 
+        {
+
+        }
+    }
+}
